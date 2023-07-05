a1406e70c9bb761c1c07a852d63a345ff756aa76
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-04 20:07:16 -0600

    workoutsessions files and migrations

---
+++ workoutsessions/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Session(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  workout_date = models.DateField()
+
+  def __str__(self):
+    return f"Sesión de {self.user.username} - {self.workout_date}"
