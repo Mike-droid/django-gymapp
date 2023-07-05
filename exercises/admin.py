2196147b9f8cb2abb0ed4b38f994f8776b5272a1
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-05 11:33:31 -0600

    Registradas entidades en django admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Exercise
 
-# Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+  pass
+
+
+admin.site.register(Exercise, ExerciseAdmin)
--- registerworkouts/admin.py
+++ registerworkouts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import RegisterWorkout
 
-# Register your models here.
+
+class RegisterWorkoutAdmin(admin.ModelAdmin):
+  pass
+
+
+admin.site.register(RegisterWorkout, RegisterWorkoutAdmin)
