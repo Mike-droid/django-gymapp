f43e69aaeef443f80d1cae861621324b2ad9a1a6
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-09 21:52:35 -0600

    only showing users on a workout session

--- registerworkouts/models.py
+++ registerworkouts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 from workoutsessions.models import Session
 from exercises.models import Exercise
+
 
 class RegisterWorkout(models.Model):
     session = models.ForeignKey(Session, on_delete=models.CASCADE)
@@ -8,6 +10,8 @@ class RegisterWorkout(models.Model):
     weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     series = models.PositiveIntegerField()
     repetitions = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return f"Registro de {self.exercise.name} - Sesión: {self.session}"
--- registerworkouts/views.py
+++ registerworkouts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import RegisterWorkout
 
 
+@login_required
 def register_workouts(request):
-  return render(request, 'register_workouts.html')
+  user = request.user
+  workouts = RegisterWorkout.objects.filter(session__user=user)
+  return render(request, 'register_workouts.html', {
+    'workouts': workouts
+  })
