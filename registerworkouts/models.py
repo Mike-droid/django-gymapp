e259b3f202dfe5ece6fc139c0398ae47aca90f7f
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-10 21:42:08 -0600

    showing workouts

--- registerworkouts/models.py
+++ registerworkouts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from workoutsessions.models import Session
+from workoutsessions.models import Workout_Session
 from exercises.models import Exercise
 
 
 class RegisterWorkout(models.Model):
-    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    session = models.ForeignKey(Workout_Session, on_delete=models.CASCADE)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
     weight = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     series = models.PositiveIntegerField()
